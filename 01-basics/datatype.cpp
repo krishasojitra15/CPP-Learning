d21b048b5894beefd7c9e56bc61938c5a369d98f
krishasojitra15 <krishasojitra15@gmail.com>
2026-07-10 19:26:50 +0530

    Day 2: datatype

---
+++ 01-basics/datatype.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age = 18;
+    char grade = 'A';
+    float pi = 3.14f;
+    bool issafe = true;
+    double price = 100.99;
+
+    cout << age << "\n " << grade << "\n " << pi << "\n " << issafe << "\n " << price << " " << endl;
+
+    return 0;
+}

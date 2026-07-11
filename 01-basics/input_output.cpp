f9ac77f72989ae06c8efc3c4d82c1a518d9d08b7
krishasojitra15 <krishasojitra15@gmail.com>
2026-07-11 17:43:40 +0530

    Day 2: Input and Output in C++

---
+++ 01-basics/input_output.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    double price;
+    cout << "enter the price: ";
+    cin >> price;
+
+    cout << "you entered price = " << price << endl;
+    return 0;
+}

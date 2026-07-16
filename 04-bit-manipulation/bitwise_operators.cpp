fe3ffb3f1700932326db1fe45ad97de68c1cdf16
krishasojitra15 <krishasojitra15@gmail.com>
2026-07-16 23:27:38 +0530

    Day 7: Bitwise Manipulation in C++

---
+++ 04-bit-manipulation/bitwise_operators.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+  int a=4, b=8;
+  cout << (a&b) <<endl;
+  cout << (a | b) << endl;
+  cout << (a ^ b) << endl;
+  cout << (a << b) << endl;
+  cout << (a >> b) << endl;
+  
+//   
+cout << (4 * 5 %2) << endl;
+
+  return 0; 
+}

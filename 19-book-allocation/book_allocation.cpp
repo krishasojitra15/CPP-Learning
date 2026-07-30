1664da0ddbc3b3a9a7f43c8496cd37b1a372b7c8
krishasojitra15 <krishasojitra15@gmail.com>
2026-07-30 15:54:00 +0530

    Day 19: Solved Book Allocation Problem using Binary Search on Answer

---
+++ 19-book-allocation/book_allocation.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+#include<vector>
+using namespace std;  
+
+bool isvalid(vector<int> &arr,int n, int m, int maxallowedpages){
+    int students=0, pages=0;
+
+    for(int i=0; i<n; i++){
+        if(arr[i] > maxallowedpages){
+            return false;
+        }
+
+        if(pages + arr[i] <= maxallowedpages){
+            pages += arr[i];
+
+        }else{
+            students++;
+            pages =arr[i];
+        }
+    }
+    return students > n? false : true;
+}
+
+int allocatebooks(vector<int> &arr,int n,int m){
+    if(m >n){
+        return -1;
+    } 
+
+    int sum=0;
+    for(int i=0; i<n ; i++){
+        sum += arr[i];
+    }
+    int ans =-1;
+    int st =0, end=sum;
+
+    while(st <= end){
+        int mid =st +(end-st)/2;
+
+        if(isvalid(arr,n,m,mid)){
+            ans = mid;
+            end = mid-1;
+
+        } else {
+            st = mid +1;
+        }
+    } 
+    return ans;
+}
+
+int main(){
+    vector<int> arr ={2,1,3,4};
+    int n =4, m=2;
+
+    cout << allocatebooks(arr,n,m) << endl;
+    return 0;
+}

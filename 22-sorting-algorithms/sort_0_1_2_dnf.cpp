452b2be014871d13e7f4b2adc1528cd4a36c9d89
krishasojitra15 <krishasojitra15@gmail.com>
2026-08-03 17:55:44 +0530

    Add Sort 0s 1s 2s using Counting and Dutch National Flag Algorithm

---
+++ 22-sorting-algorithms/sort_0_1_2_dnf.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void sortcolours(vector<int> &nums){
+    int n = nums.size();
+    int low = 0,mid=0, high = n-1;
+
+    while(mid <= high){
+        if(nums [mid] ==0){
+            swap(nums[low], nums[mid]);
+            mid++; low++; 
+
+    }else if(nums[mid]==1){
+        mid++;
+    }else { 
+        swap(nums[high],nums[mid]);
+        high--;
+}
+}
+
+    

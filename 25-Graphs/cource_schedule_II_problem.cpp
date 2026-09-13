9d294704eff0cc63d1d3009fb8c1110f77a8ef4b
krishasojitra15 <krishasojitra15@gmail.com>
2026-09-13 15:16:01 +0530

    cource schedule II problem

---
+++ 25-Graphs/cource_schedule_II_problem.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <list>
+#include<stack>
+using namespace std;
+class graph{
+    public:
+    bool iscycleDFS(int src, vector<bool> &vis,vector<bool> &recpath,vector<vector<int>> &edges){
+        vis[src] = true;
+        recpath[src] = true;
+
+        for(int i=0;i<edges.size();i++){
+            int v = edges[i][0];
+            int u = edges[i][1];
+
+            if(u ==src){
+                if(!vis[v]){
+                    if(iscycleDFS(v,vis,recpath,edges)){
+                        return true;
+                    } 
+                }else if(recpath[v]){
+                    return true;
+                }
+            }
+        }
+        recpath[src]=false;
+        return false;
+    }
+
+    void topoorder(int src,vector<bool> &vis,stack<int> &s,vector<vector<int>>&edges){
+        vis[src] = true;
+
+        for(int i=0;i<edges.size();i++){
+            int v=edges[i][0];
+            int u=edges[i][1];
+
+            if(src == u){
+                if(!vis[v]){
+                    topoorder(v,vis,s,edges);
+                }
+            }
+        }
+        s.push(src);
+    }
+    vector<int>findorder(int n,vector<vector<int>> &edges){
+        vector<bool> vis(n,false);
+        vector<bool> recpath(n,false);
+        vector<int> ans;
+
+        for(int i=0; i<n; i++){
+            if(!vis[i]){
+                if(iscycleDFS(i,vis,recpath,edges)){
+                    return ans;
+                }
+            }   
+        }
+      stack<int> s;
+      vis.assign(n,false);
+
+      for(int i =0; i<n;i++){
+        if(!vis[i]){
+            topoorder(i,vis,s,edges);
+        }
+      }
+
+      while(s.size() >0){
+        ans.push_back(s.top());
+        s.pop();
+      }
+      return ans;
+    }
+};

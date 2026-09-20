b09198b3db3767ff076f66882ca1697f33395005
krishasojitra15 <krishasojitra15@gmail.com>
2026-09-20 18:41:40 +0530

    disjoint set union

---
+++ 25-Graphs/disjoint_set_union.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <list>
+#include<stack>
+#include<climits>
+using namespace std;
+
+class Disjointsetunion {
+    public:
+    int n;
+    vector<int> par, rank;
+
+    Disjointsetunion(int n){
+        this->n = n;
+
+        for(int i=0; i<n; i++){
+            par.push_back(i);
+            rank.push_back(0);
+        }
+    }
+
+    void unionByRank(int a, int b){
+        int parA = find(a);
+        int parB = find(b);
+
+        if(parA == parB) return;
+
+        if(rank[parA]  == rank[parB]){
+            par[parB] = parA;
+            rank[parA]++;
+        }else if(rank[parA] > rank[parB]){
+            par[parB] = parA;
+        }else{
+            par[parA] =parB;
+        }
+    }
+
+    void getInfo(){
+        for(int i=0;i<n;i++){
+            cout << rank[i] << " ";
+        }
+        cout << endl;
+    }
+
+    int find(int x){
+        if(par[x] == x){
+            return x;
+        }
+
+        return find(par[x]);
+    }
+};
+
+int main() {
+    Disjointsetunion dsu(6);
+
+    dsu.unionByRank(0,2);
+    cout << dsu.find(2) << endl;
+
+    dsu.unionByRank(1,3);
+    dsu.unionByRank(2,5);
+    dsu.unionByRank(0,3);
+    cout << dsu.find(2) << endl;
+
+    dsu.unionByRank(0,4);
+    dsu.getInfo();
+
+    return 0;
+}
